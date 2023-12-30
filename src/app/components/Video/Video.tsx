c4168f52853799e98f897f0f3e67fc1cbef1ce6a
Igor Gorlov <gorlov35@gmail.com>
2023-12-30 12:23:51 +0300

    feat: add video

--- src/app/components/Video/Video.tsx
+++ src/app/components/Video/Video.tsx
@@ -1,5 +1,19 @@
+import { cn } from '@/app/libs/utils';
+import videoPlaceholder from '@images/general/video/video-placeholder.png';
+import Image from 'next/image';
+import Container from '../container/Container';
 import styles from './Video.module.css';
 
+// console.log(videoPlaceholder);
+
 export function Video() {
-	return <div className={styles.Video}></div>;
+	return (
+		<div className={styles.video}>
+			<Container className={styles.container}>
+				<div className={cn(styles.videoContainer)}>
+					<Image src={videoPlaceholder} alt="video" />
+				</div>
+			</Container>
+		</div>
+	);
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,14 @@ import { Contacts } from './components/Contacts';
 import { First } from './components/First';
 import MainLayout from './components/mainLayout/MainLayout';
 import { More } from './components/More';
+import { Video } from './components/Video';
 
 export default function Home() {
 	return (
 		<MainLayout>
 			<First />
 			<About />
+			<Video />
 			<More />
 			<Contacts />
 		</MainLayout>
